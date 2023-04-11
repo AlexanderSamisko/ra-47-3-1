c58c5cdbd596b2d6131fc9dafc4de7a9bee66f4b
AlexanderSamisko <83254509+AlexanderSamisko@users.noreply.github.com>
2023-04-11 11:07:08 +0300

    second thought

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -7,7 +7,7 @@ export default function Stars({ count }) {
     
     let newCount = Array.from({length: count}, (_, i) => i + 1);
 
-    return <ul className="star-field">
+    return <ul className="card-body-stars u-clearfix">
         {
            count >= 0 && count <= 5 && newCount.map((item, index) => <Star key={index} value={item} />)
         }
